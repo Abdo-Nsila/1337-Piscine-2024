8b389ab1f94cae355662ba340c52b8f2d2d0a775
Abdo-Nsila <abdonsila2004@gmail.com>
2024-06-28 00:54:08 +0100

    feat: Add ft_print_reverse_alphabet function

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 21:19:42 by abnsila           #+#    #+#             */
+/*   Updated: 2024/06/27 21:24:52 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	alphabet;
+
+	alphabet = 122;
+	while (alphabet >= 97)
+	{
+		write (1, &alphabet, 1);
+		alphabet--;
+	}
+}
