ad0a5c911461eeb6791935d7e75186e5cddb2ee3
Abdo-Nsila <abdonsila2004@gmail.com>
2024-06-29 11:41:20 +0100

    add ex08

---
+++ C00/ex08/ft_print_combn.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 10:18:58 by abnsila           #+#    #+#             */
+/*   Updated: 2024/06/29 10:57:49 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putresult(int *tab, int n)
+{
+	int		i;
+	bool	low;
+
+	i = 1;
+	low = 1;
+	while (i < n)
+	{
+		if (tab[i - 1] >= tab[i])
+			low = 0;
+		i++;
+	}
+	if (low)
+	{
+		i = 0;
+		while (i < n)
+			ft_putchar(tab[i++] + 48);
+		if (tab[0] < (10 - n))
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int		i;
+	int		tab[n];
+
+	i = 0;
+	while (i < n)
+		tab[i++] = 0;
+	while (tab[0] <= (10 - n) && n >= 1 && n < 10)
+	{
+		ft_putresult(tab, n);
+		tab[n - 1]++;
+		i = n - 1;
+		while (i && n > 1)
+		{
+			if (tab[i] > 9)
+			{
+				tab[i - 1]++;
+				tab[i] = 0;
+			}
+			i--;
+		}
+	}
+}
