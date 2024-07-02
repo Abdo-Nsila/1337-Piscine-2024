3f8fc825d6d8bc96dcf8f26b99c5367c6522c66d
Abdo-Nsila <abdonsila2004@gmail.com>
2024-07-02 13:22:18 +0100

    Add ex11

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,17 +6,33 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:42:27 by abnsila           #+#    #+#             */
-/*   Updated: 2024/07/01 11:58:55 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/07/02 12:01:11 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr_non_printable(char *str)
 {
 	int i;
+	char hex[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
 	i = 0;
-	while ()
+	while (str[i] != '\0') 
 	{
-
+		if ((str[i] >= 0 && str[i] <= 31) || str[i] == 127)
+		{
+			ft_putchar('\\');
+			ft_putchar((int)hex[str[i] / 16]);
+			ft_putchar((int)hex[str[i] % 16]);
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
 	}
 }
