66ca35cd092d9e3d89dbf3ae99709c4115dee314
Abdo-Nsila <abdonsila2004@gmail.com>
2024-07-17 14:22:47 +0100

    Update Shell01

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -6,14 +6,13 @@
 #    By: kamado <abnsila@student.1337.ma>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/14 18:12:48 by kamado            #+#    #+#              #
-#    Updated: 2024/07/14 18:12:57 by kamado           ###   ########.fr        #
+#    Updated: 2024/07/17 12:25:56 by abnsila          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 rm -f libft.a
 rm -f ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
-gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
-ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
-ranlib libft.a
+cc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
 rm -f ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d':' -f1 | rev | sort -r | awk 'NR>=ENVIRON["FT_LINE1"] && NR<=ENVIRON["FT_LINE2"]' | paste -sd, - | sed 's/$/./'
+cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d':' -f1 | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | awk '{printf "%s%s", sep, $0; sep=", ";} END{print "."}' | tr -d '\n'
