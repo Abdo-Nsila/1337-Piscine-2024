7375815b0ead34bcca4c50c97f09a53e49a7a8bd
Abdellah Nsila <126418751+Abdo-Nsila@users.noreply.github.com>
2024-07-27 17:54:49 +0100

    Update square.c

--- BSQ/square.c
+++ BSQ/square.c
@@ -4,7 +4,8 @@
 
 #define BUFFER_SIZE 256
 
-int main() {
+int main() 
+{
     int fd = open("file", O_RDONLY);
     if (fd == -1) {
         write(2, "Could not open file\n", 20);
@@ -13,17 +14,18 @@ int main() {
 
     char buffer[BUFFER_SIZE];
     ssize_t bytesRead;
-    while ((bytesRead = read(fd, buffer, BUFFER_SIZE - 1)) > 0) {
+    while ((bytesRead = read(fd, buffer, BUFFER_SIZE - 1)) > 0) 
+    {
         buffer[bytesRead] = '\0';
         write(1, buffer, bytesRead);
     }
 
-    if (bytesRead == -1) {
+    if (bytesRead == -1) 
+    {
         write(2, "Error reading file\n", 18);
         exit(1);
     }
 
     close(fd);
-
     return 0;
 }
